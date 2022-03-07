4306c4d3705d007905a3e17b94d34f04604b1957
French <77544281+french08@users.noreply.github.com>
2022-03-07 15:31:33 +0100

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,49 @@
+const { MessageEmbed } = require('discord.js'); 
+const {default_prefix, color, salonready} = require ('../config.json');
+
+ module.exports.run = (client) => { 
+
+     console.log(`${client.user.tag} est prêt !`);
+     
+
+ const statuses = [
+
+        () => '&help',
+
+        () => `${Math.round(client.ws.ping)}ms`,        
+
+        () => `${client.guilds.cache.size} Serveurs` 
+
+  ]
+
+let i = 0
+
+setInterval(() => {  
+
+  client.user.setActivity(statuses[i](), {type: 'STREAMING'});  
+
+  i = ++i % statuses.length
+
+}, 10000)
+     
+  const embed = new MessageEmbed() 
+
+         .setTitle(`${client.user.tag}`)
+  
+         .setDescription("Je suis prêt à être utilisé !")
+
+         
+
+         .setColor(color) 
+
+         .setTimestamp();
+     client.channels.fetch(salonready)
+
+    .then(channel => {
+
+        channel.send(embed);
+         
+
+     });
+          };
+ 
